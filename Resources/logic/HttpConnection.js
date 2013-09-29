ddf65b55ffb8ecb3736c57b04405ef8c68d9437e
Qian Shen <199378769@qq.com>
2013-09-29 02:37:09 -0400

    connection

--- Resources/logic/HttpConnection.js
+++ Resources/logic/HttpConnection.js
@@ -198,5 +198,26 @@ function getTopicsByFriends(userID){
 }
 
 function getFriends(){
-	
+	responseCode = 0;
+	var client = Ti.Network.createHTTPClient({
+		// function called when the response data is available
+		onload : function(e) {
+			    Ti.API.info('GOT ' + this.responseText);
+				Ti.App.Properties.setString('FriendList', this.responseText);
+				responseCode = 1;
+		},
+		// function called when an error occurs, including a timeout
+		onerror : function(e) {
+			Ti.API.debug(e.error);
+			responseCode = -2;
+			//alert('error');
+		},
+		timeout : 5000 // in milliseconds
+	});
+	// Prepare the connection.
+	var url = URL_PREFIX + "f=showFriends&userID=" + getUserID();
+	Titanium.API.info(url);
+	client.open("GET", url);
+	// Send the request.
+	client.send();
 }
